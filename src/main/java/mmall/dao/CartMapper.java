86b4106b5eed742bc88b2aae3a666f345d96c66a
wyw <1280245806@qq.com>
2018-10-16 16:03:26 +0800

    project init

---
+++ src/main/java/mmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package mmall.dao;
+
+import mmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/mmall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package mmall.dao;
+
+import mmall.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
